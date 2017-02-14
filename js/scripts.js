8ba141394f99d264af7607b32098b7d9d96a47d6
Rob McKenzie and Tom Yoder <tom@>
2017-02-14 11:24:50 -0800

    final js, css, html

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,30 @@ $(document).ready(function(){
     $("#octo-show").slideToggle();
     $("#octo-hide").slideToggle();
   });
+  $("button#green").click(function() {
+      $("body").removeClass();
+      $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function() {
+      $("body").removeClass();
+      $("body").addClass("yellow-background");
+  });
+  $("button#red").click(function() {
+      $("body").removeClass();
+      $("body").addClass("red-background");
+  });
+  $("button#bgDark").click(function() {
+      $("body").removeClass();
+      $("body").addClass("dark-background");
+  });
+  $("button#bgLight").click(function() {
+      $("body").removeClass();
+      $("body").addClass("light-background");
+  });
+
+  $(".clickable2").click(function() {
+    $("#warning-show").toggle();
+    $("#warning-hide").toggle();
+  });
+
 });
